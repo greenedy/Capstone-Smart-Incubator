5b9f3817ae4e8bb0366cc2725fb852cea763e7a8
Jessica <albinosilver@hotmail.com>
2019-02-02 21:03:56 -0500

    added some basic login/preset page

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+
+    else:
+        return "Welcome to Pincubator!"
+
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'username':
+        session['logged_in'] = True
+
+    else:
+        flash('wrong password!')
+    return home()
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+app.run(debug=True, host='127.0.0.1', port=8080)
