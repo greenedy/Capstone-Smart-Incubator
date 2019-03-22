4d8e163de3656f1400c3c05c51b51183e9774529
Dylan <greenedy@github.com>
2019-03-22 03:34:26 -0400

    -more fixing travis unit test

--- test/test_get_data.py
+++ test/test_get_data.py
@@ -1,7 +1,11 @@
+import unittest
 from unittest import TestCase
-
 
 class TestGet_data(TestCase):
     def test_get_data(self):
         '''Testing'''
         self.assertEqual(1, 2)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
