d9b19bbb187da13054509577319ffff4e725a814
Jessica <albinosilver@hotmail.com>
2019-03-29 09:46:08 -0400

    Modifications to incubator script so it can run on the pi

--- scripts/incubator.py
+++ scripts/incubator.py
@@ -1,66 +1,83 @@
-import mysql.connector
-import Adafruit_DHT
-import os
+#!/usr/bin/python
+
+import json
 import sys
 import time
+import datetime
+import Adafruit_DHT
+import os
+import mysql.connector
 import RPi.GPIO as GPIO
 
-# Setup the relay connection
+# Install the following dependencies:
+# sudo pip3 install Adafruit_DHT
+# sudo apt-get install python3-mysql.connector
+
+# Setup power relay
 GPIO.setmode(GPIO.BCM)
 PWR = 26
 GPIO.setwarnings(False)
-GPIO.setup(PWR,GPIO.OUT)
-
-# Type of sensor
-DHT_TYPE = Adafruit_DHT.AM2302
-
-# Example of sensor connected to Raspberry Pi pin 23
-DHT_PIN  = 4
-
-# Connect to database
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="password", database="smartincubator")
-
-mycursor = mydb.cursor()
-
-# How long to wait (in seconds) between measurements.
-FREQUENCY_SECONDS = 3
-
-# Set the power state of the relay
+GPIO.setup(PWR, GPIO.OUT)
 RELAY_POWER = 'OFF'
 
-while True:
+# Setup sensor
+DHT_TYPE = Adafruit_DHT.AM2302
+DHT_PIN = 14
+FREQUENCY_SECONDS = 3
 
-    # Attempt to get sensor reading.
-    humidity, temperature = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
+# Connect to the database
+mydb = mysql.connector.connect(host="localhost", user="pi", passwd="klcmc123", database="smartincubator")
+cursor = mydb.cursor()
+
+running = True;
+while running:
+    os.system('clear')
+    print("Smart Incubator")
+    # connect to database
+    # find running config
+    print("Retrieving configuration...")
+    cursor.execute("SELECT temperature FROM configurations WHERE running = 1;")
+    current = cursor.fetchone()
+    threshold = current[0]
+
+    if (len(current) == 0):
+        print("No running configuration found. Aborting.")
+        running = False;
+        break;
+    print("Configuration loaded.")
+
+    # Attempt to get the sensor readings
+    print("Retrieving sensor readings...")
+    humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
 
     # Skip to the next reading if a valid measurement couldn't be taken.
-    # This might happen if the CPU is under a lot of load and the sensor
-    # can't be reliably read (timing is critical to read the sensor).
-    if humidity is None or temperature is None:
+    # This might happen if the CPU is under a lot of load and the sensor can't be reliably read.
+    if humidity is None or temp is None:
+        print("No humidity or temperature data returned. Retrying in 2 seconds.")
         time.sleep(2)
         continue
 
-    if (temperature > 38.0):
+    # Check the sensor readings
+    print("Sensor data found. Processing...")
+    if (temp > int(threshold)):
         GPIO.output(PWR, False)
         RELAY_POWER = 'OFF'
-    elif (temperature < 37.0):
+    elif (temp <= int(threshold)):
         GPIO.output(PWR, True)
         RELAY_POWER = 'ON'
 
-    print("Smart Incubator")
-    try:
+    # Print the current readings
+    # os.system('clear')
+    print('Temperature: {0:0.1f} C'.format(temp))
+    print('Humidity:    {0:0.1f} %'.format(humidity))
+    print('Relay Power: {0}'.format(RELAY_POWER))
+    print('Press Ctrl-C to quit.')
 
-        sql = "INSERT INTO incubator (humidity, temperature) VALUES (%s, %s)"
-        val = (humidity, temperature)
-        mycursor.execute(sql, val)
-
-        mydb.commit()
-
-        print(mycursor.rowcount, "record inserted.")
-
-    except:
-        time.sleep(FREQUENCY_SECONDS)
-        continue
+    # Add the readings to the database
+    print("Inserting data into the database...")
+    query = "INSERT INTO test(temperature,humidity,power) VALUES(%s,%s,%s)"
+    cursor.execute(query, (temp, humidity, RELAY_POWER))
+    mydb.commit()
 
     # Wait before continuing
     time.sleep(FREQUENCY_SECONDS)
