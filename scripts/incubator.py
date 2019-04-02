cce3314b639c103e58d410f49b139e5a2fcf5438
Jessica <albinosilver@hotmail.com>
2019-04-02 13:31:25 -0400

    Modified incubator script to run in a loop

--- scripts/incubator.py
+++ scripts/incubator.py
@@ -29,86 +29,116 @@ FREQUENCY_SECONDS = 3
 mydb = mysql.connector.connect(host="localhost", user="pi", passwd="klcmc123", database="smartincubator")
 cursor = mydb.cursor(buffered=True)
 
-running = True;
-while running:
-    os.system('clear')
-    print("Smart Incubator")
-    # connect to database
-    # find running config
-    print("Retrieving configuration...")
-    cursor.execute("SELECT temperature, humidity FROM configurations WHERE running = 1;")
-    current = cursor.fetchone()
-    temperatureThreshold = current[0]
-    humidityThreshold = current[1]
+runningFlag = False
 
-    if (len(current) == 0):
-        print("No running configuration found. Aborting.")
-        running = False;
-        break;
-    print("Configuration loaded.")
 
-    # Attempt to get the sensor readings
-    print("Retrieving sensor readings...")
-    humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
+def waiting(runningFlag):
+    while not runningFlag:
+        print("Waiting...")
+        cursor.execute("SELECT temperature, humidity FROM configurations WHERE running = 1;")
+        current = cursor.fetchone()
+        if (current is None):
+            print("No running configuration found. Waiting.")
+            time.sleep(2)
+        else:
+            print("Starting running...")
+            runningFlag = True
+            running(runningFlag)
 
-    # Skip to the next reading if a valid measurement couldn't be taken.
-    # This might happen if the CPU is under a lot of load and the sensor can't be reliably read.
-    if humidity is None or temp is None:
-        print("No humidity or temperature data returned. Retrying in 2 seconds.")
-        time.sleep(2)
-        continue
 
-    # Check the sensor readings
-    print("Sensor data found. Processing...")
-    if (temp > int(temperatureThreshold)):
-        GPIO.output(PWR, False)
-        RELAY_POWER = 'OFF'
-    elif (temp <= int(temperatureThreshold)):
-        GPIO.output(PWR, True)
-        RELAY_POWER = 'ON'
+def running(runningFlag):
+    while runningFlag:
+        os.system('clear')
+        print("Smart Incubator")
+        # connect to database
+        # find running config
+        print("Retrieving configuration...")
+        cursor.execute("SELECT temperature, humidity FROM configurations WHERE running = 1;")
+        current = cursor.fetchone()
+        print(current)
+        if (current is None):
+            print("No running configuration found. Return to wait")
+            runningFlag = False;
+            waiting(runningFlag)
+        else:
+            print("Configuration loaded.")
 
-    # Check if temperature notification needs to be sent
-    if (temp > int(temperatureThreshold) + 3):
-        print("Inserting temperature notification into the database...")
-        query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
-        cursor.execute(query, ("Incubator temperature is " + str(temp) + " °C", "Incubator temperature is " + str(temp) + " °C", "error"))
-        mydb.commit()
-    elif (temp <= int(temperatureThreshold) - 3):
-        print("Inserting temperature notification into the database...")
-        query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
-        cursor.execute(query, ("Incubator temperature is " + str(temp) + " °C", "Incubator temperature is " + str(temp) + " °C", "error"))
-        mydb.commit()
+            temperatureThreshold = current[0]
+            humidityThreshold = current[1]
 
-    # Check if humidity notification needs to be sent
-    if (humidity > int(humidityThreshold) + 3):
-        print("Inserting humidity notification into the database...")
-        query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
-        cursor.execute(query, ("Incubator humidity is " + str(humidity) + " %", "Incubator humidity is " + str(humidity) + " %", "error"))
-        mydb.commit()
-    elif (humidity <= int(humidityThreshold) - 3):
-        print("Inserting humidity notification into the database...")
-        query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
-        cursor.execute(query, ("Incubator humidity is " + str(humidity) + " %", "Incubator humidity is " + str(humidity) + " %", "error"))
-        mydb.commit()
+            # Attempt to get the sensor readings
+            print("Retrieving sensor readings...")
+            humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
 
-    # Print the current readings
-    # os.system('clear')
-    print('Temperature: {0:0.1f} C'.format(temp))
-    print('Humidity:    {0:0.1f} %'.format(humidity))
-    print('Relay Power: {0}'.format(RELAY_POWER))
-    print('Press Ctrl-C to quit.')
+            # Skip to the next reading if a valid measurement couldn't be taken.
+            # This might happen if the CPU is under a lot of load and the sensor can't be reliably read.
+            if humidity is None or temp is None:
+                print("No humidity or temperature data returned. Retrying in 2 seconds.")
+                time.sleep(2)
+                continue
 
-    # Add the readings to the database
-    print("Inserting data into the database...")
-    query = "INSERT INTO incubator(temperature,humidity,power) VALUES(%s,%s,%s)"
-    cursor.execute(query, (temp, humidity, RELAY_POWER))
-    mydb.commit()
+            # Check the sensor readings
+            print("Sensor data found. Processing...")
+            if (temp > int(temperatureThreshold)):
+                GPIO.output(PWR, False)
+                RELAY_POWER = 'OFF'
+            elif (temp <= int(temperatureThreshold)):
+                GPIO.output(PWR, True)
+                RELAY_POWER = 'ON'
 
-    # delete readings in database that are over 24 hours old
-    print("Deleting data in the database that is older than 24 hours...")
-    query = "DELETE FROM incubator WHERE timestamp < NOW() - INTERVAL 1 DAY"
-    cursor.execute(query)
-    mydb.commit()
+            # Check if temperature notification needs to be sent
+            if (temp > int(temperatureThreshold) + 3):
+                print("Inserting temperature notification into the database...")
+                query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
+                cursor.execute(query, (
+                "Incubator temperature is " + str(temp) + " °C", "Incubator temperature is " + str(temp) + " °C",
+                "error"))
+                mydb.commit()
+            elif (temp <= int(temperatureThreshold) - 3):
+                print("Inserting temperature notification into the database...")
+                query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
+                cursor.execute(query, (
+                "Incubator temperature is " + str(temp) + " °C", "Incubator temperature is " + str(temp) + " °C",
+                "error"))
+                mydb.commit()
 
-    # Wait before continuing
-    time.sleep(FREQUENCY_SECONDS)
+            # Check if humidity notification needs to be sent
+            if (humidity > int(humidityThreshold) + 3):
+                print("Inserting humidity notification into the database...")
+                query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
+                cursor.execute(query, (
+                "Incubator humidity is " + str(humidity) + " %", "Incubator humidity is " + str(humidity) + " %",
+                "error"))
+                mydb.commit()
+            elif (humidity <= int(humidityThreshold) - 3):
+                print("Inserting humidity notification into the database...")
+                query = "INSERT INTO notifications(title,text,type) VALUES(%s,%s,%s)"
+                cursor.execute(query, (
+                "Incubator humidity is " + str(humidity) + " %", "Incubator humidity is " + str(humidity) + " %",
+                "error"))
+                mydb.commit()
+
+            # Print the current readings
+            # os.system('clear')
+            print('Temperature: {0:0.1f} C'.format(temp))
+            print('Humidity:    {0:0.1f} %'.format(humidity))
+            print('Relay Power: {0}'.format(RELAY_POWER))
+            print('Press Ctrl-C to quit.')
+
+            # Add the readings to the database
+            print("Inserting data into the database...")
+            query = "INSERT INTO incubator(temperature,humidity,power) VALUES(%s,%s,%s)"
+            cursor.execute(query, (temp, humidity, RELAY_POWER))
+            mydb.commit()
+
+            # delete readings in database that are over 24 hours old
+            print("Deleting data in the database that is older than 24 hours...")
+            query = "DELETE FROM incubator WHERE timestamp < NOW() - INTERVAL 1 DAY"
+            cursor.execute(query)
+            mydb.commit()
+
+            # Wait before continuing
+            time.sleep(FREQUENCY_SECONDS)
+
+
+waiting(runningFlag)
